6968879380cfa1326c73e84275a00a9447dfe12c
bkbarath <pandib328@gmail.com>
2024-07-28 23:25:25 +0530

    [done] Reverse integer power

--- src/basics/ReverseIntegerPower.java
+++ src/basics/ReverseIntegerPower.java
@@ -2,17 +2,22 @@ package basics;
 
 public class ReverseIntegerPower {
 
-    static long power(int N, int M){
-        long power = 1L;
-
-        for(int i =0; i<M;i++ ){
-            power =power*N;
+    static long power(int N, int R) {
+        if (R == 0) {
+            return 1;
         }
-        return power;
+
+        long mod = (long) 1e9 + 7;
+        long temp = power(N, R / 2);
+        if (R % 2 == 0) {
+            return (temp * temp) % mod;
+        } else {
+            return ((temp * temp) % mod * N) % mod;
+        }
     }
 
 
-    public static void main(String[] args){
-        System.out.println(power(12,21));
+    public static void main(String[] args) {
+        System.out.println(power(12, 21));
     }
 }
